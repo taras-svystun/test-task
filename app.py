00828b272275b3e7d01527aec73fc9793dd541f1
taras-svystun <tatarik.sv@gmail.com>
2023-10-24 21:22:00 +0300

    test

--- app.py
+++ app.py
@@ -70,7 +70,10 @@ st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 # Function for generating LLaMA2 response
 # Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_response(prompt_input):
-    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    string_dialogue = """
+    You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'.
+    Answer all the questions in short and clearly.
+    """
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
@@ -93,11 +96,6 @@ if st.session_state.messages[-1]["role"] != "assistant":
         with st.spinner("Thinking..."):
             response = generate_response(prompt)
             placeholder = st.empty()
-            # full_response = ''
-            # for item in response:
-            #     st.title(item)
-            #     full_response += item
-            #     placeholder.markdown(full_response)
             placeholder.markdown(response)
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
