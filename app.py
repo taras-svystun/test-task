a1b21c81d060ceb68fd9e0fec0683cd94b16c5bd
taras-svystun <tatarik.sv@gmail.com>
2023-10-24 21:03:04 +0300

    test

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def generate_response(prompt_input):
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
 
     query = f"{string_dialogue} {prompt_input} Assistant: "
-    output = qa.run(prompt_input)
+    output = qa.run(query)
     # output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
     #                        input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
     #                               "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
