43b62d9efc1990491d696683eec4c1ef99466a75
taras-svystun <tatarik.sv@gmail.com>
2023-10-24 11:25:32 +0300

    Rearanged function calls in

--- app.py
+++ app.py
@@ -14,12 +14,43 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
+hf_token = os.environ['HuggingFaceHub_API_Token']
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf",
+                                        token=hf_token,
+                                        #   low_cpu_mem_usage=True
+                                        )
+
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf",
+                                            device_map='auto',
+                                            torch_dtype=torch.float32,
+                                            token=hf_token,
+                                            #  load_in_8bit=True,
+                                            #  low_cpu_mem_usage=True
+                                            )
+
+pipe = pipeline("text-generation",
+                model=model,
+                tokenizer= tokenizer,
+                torch_dtype=torch.bfloat16,
+                device_map="auto",
+                max_new_tokens = 1024,
+                do_sample=True,
+                top_k=10,
+                num_return_sequences=1,
+                eos_token_id=tokenizer.eos_token_id
+                )
+
+llm=HuggingFacePipeline(pipeline=pipe, model_kwargs={'temperature':0})
+
+
 
 st.title('Demo for <client> 😇️️️️️️')
 
 uploaded_file = st.file_uploader("Add a text file")
 if uploaded_file is not None:
-    hf_token = os.environ['HuggingFaceHub_API_Token']
+    
 
     with open('_sample.txt', 'w') as file:
         file.write("".join([line.decode() for line in uploaded_file]))
@@ -31,41 +62,15 @@ if uploaded_file is not None:
                                         chunk_size=1000,
                                         chunk_overlap=50)
     text_chunks=text_splitter.split_documents(documents)
-
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
-
     vectorstore=FAISS.from_documents(text_chunks, embeddings)
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf",
-                                            token=hf_token,
-                                            #   low_cpu_mem_usage=True
-                                            )
 
-    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf",
-                                                device_map='auto',
-                                                torch_dtype=torch.float32,
-                                                token=hf_token,
-                                                #  load_in_8bit=True,
-                                                #  low_cpu_mem_usage=True
-                                                )
-
-    pipe = pipeline("text-generation",
-                    model=model,
-                    tokenizer= tokenizer,
-                    torch_dtype=torch.bfloat16,
-                    device_map="auto",
-                    max_new_tokens = 1024,
-                    do_sample=True,
-                    top_k=10,
-                    num_return_sequences=1,
-                    eos_token_id=tokenizer.eos_token_id
-                    )
-
-    llm=HuggingFacePipeline(pipeline=pipe, model_kwargs={'temperature':0})
     chain =  RetrievalQA.from_chain_type(llm=llm,
-                                         chain_type="stuff",
-                                         return_source_documents=True,
-                                         retriever=vectorstore.as_retriever()
-                                         )
+                                        chain_type="stuff",
+                                        return_source_documents=True,
+                                        retriever=vectorstore.as_retriever()
+                                        )
+
+    
     # Try Conversational Retrieval
 
 
