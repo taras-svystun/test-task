cababba9a6c69b384cda68680a165ccfa3d8d90a
taras-svystun <tatarik.sv@gmail.com>
2023-10-24 20:09:53 +0300

    Added qa definition

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 import replicate
 import streamlit as st
+from langchain.llms import OpenLLM
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import FAISS
 from langchain.document_loaders import TextLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 
 # App title
@@ -38,6 +42,16 @@ if uploaded_file is not None:
 
     text_chunks=text_splitter.split_documents(documents)
 
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = OpenAIEmbeddings()
+    vectorstore=FAISS.from_documents(text_chunks, embeddings)
+
+    server_url = "https://48d8-38-147-83-24.ngrok-free.app/"
+    llm = OpenLLM(server_url=server_url)
+
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever())
+
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How can I help you?"}]
@@ -53,16 +67,22 @@ st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
 # Refactored from https://github.com/a16z-infra/llama2-chatbot
-def generate_llama2_response(prompt_input):
+def generate_response(prompt_input):
     string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+
+    # query = f"{string_dialogue} {prompt_input} Assistant: "
+    # output = qa.run(query)
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
                                   "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
+    for _ in range(3): print()
+    print(output)
+    for _ in range(3): print()
     return output
 
 # User-provided prompt
@@ -75,7 +95,7 @@ if prompt := st.chat_input(disabled=not replicate_api):
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = generate_llama2_response(prompt)
+            response = generate_response(prompt)
             placeholder = st.empty()
             full_response = ''
             for item in response:
