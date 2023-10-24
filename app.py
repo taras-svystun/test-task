ae53238a951d4d405b775e66495d9477e68aa5dd
taras-svystun <tatarik.sv@gmail.com>
2023-10-24 13:32:06 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,82 +1,68 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
-import os
-import torch
-import textwrap
 import streamlit as st
-from langchain.vectorstores import FAISS
-from langchain.chains import RetrievalQA
-from langchain import HuggingFacePipeline
-from langchain.document_loaders import TextLoader
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
+import replicate
+import os
 
-hf_token = os.environ['HuggingFaceHub_API_Token']
+# App title
+st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
-st.title('Demo for <client> 😇️️️️️️')
+# Replicate Credentials
+with st.sidebar:
+    st.title('🦙💬 Llama 2 Chatbot')
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
-uploaded_file = st.file_uploader("Add a text file")
-if uploaded_file is not None:
-    
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
-    with open('_sample.txt', 'w') as file:
-        file.write("".join([line.decode() for line in uploaded_file]))
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-    loader = TextLoader('_sample.txt')
-    documents = loader.load()
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-    text_splitter=CharacterTextSplitter(separator='\n',
-                                        chunk_size=1000,
-                                        chunk_overlap=50)
-    text_chunks=text_splitter.split_documents(documents)
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
-    vectorstore=FAISS.from_documents(text_chunks, embeddings)
+# Function for generating LLaMA2 response
+# Refactored from https://github.com/a16z-infra/llama2-chatbot
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
+    return output
 
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf",
-                                            token=hf_token,
-                                            # low_cpu_mem_usage=True
-                                            )
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
-    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf",
-                                                device_map='auto',
-                                                torch_dtype=torch.float32,
-                                                token=hf_token,
-                                                #  load_in_8bit=True,
-                                                # low_cpu_mem_usage=True
-                                                )
-
-    pipe = pipeline("text-generation",
-                    model=model,
-                    tokenizer= tokenizer,
-                    torch_dtype=torch.bfloat16,
-                    device_map="auto",
-                    max_new_tokens = 1024,
-                    do_sample=True,
-                    top_k=10,
-                    num_return_sequences=1,
-                    eos_token_id=tokenizer.eos_token_id
-                    )
-
-    llm=HuggingFacePipeline(pipeline=pipe, model_kwargs={'temperature':0})
-    
-
-    chain =  RetrievalQA.from_chain_type(llm=llm,
-                                        chain_type="stuff",
-                                        return_source_documents=True,
-                                        retriever=vectorstore.as_retriever()
-                                        )
-
-    
-    # Try Conversational Retrieval
-
-
-    with st.form('form'):
-        query = st.text_area('Enter your query:', 'What is the Wizard name?')
-        submitted = st.form_submit_button('Submit')
-        if submitted:
-            result=chain({"query": query}, return_only_outputs=True)
-            wrapped_text = textwrap.fill(result['result'], width=500)
-            st.write(wrapped_text)
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
